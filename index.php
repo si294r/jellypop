9908136e98cf61d20d58647b58dfee5815cc2efc
chairuddinsyah <chairuddinsyah@10.100.8.149>
2016-07-13 11:52:06 +0700

    Update Commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $start_time = microtime(true);
 
 define('IS_DEVELOPMENT', true);
-define('JELLY_POP_TOKEN', '1234567890');
+define('JELLY_POP_TOKEN', '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ');
 
 function show_error($response_code, $status_code, $message) {
     http_response_code($response_code);
@@ -12,9 +12,6 @@ function show_error($response_code, $status_code, $message) {
 }
 
 $headers = getallheaders();
-if (!isset($headers['Content-Type']) || $headers['Content-Type'] != 'application/json') {
-    show_error(400, "400 Bad Request", "Invalid Content Type");
-}
 if (!isset($headers['Jelly-Pop-Token']) || $headers['Jelly-Pop-Token'] != JELLY_POP_TOKEN) {
     show_error(401, "401 Unauthorized", "Invalid Token");
 }
@@ -30,41 +27,44 @@ $service = isset($params[0]) ? $params[0] : "";
 
 switch ($service) {
     case 'connect' :
-        if ($_SERVER["REQUEST_METHOD"] == 'POST') {
-            $input = file_get_contents("php://input");
-            include('connect.php');
-        } else {
-            show_error(405, "405 Method Not Allowed", "Invalid Method");
-        }
-        break;
+//        if ($_SERVER["REQUEST_METHOD"] != 'POST') {
+//            show_error(405, "405 Method Not Allowed", "Invalid Method");
+//        }
+//        if (!isset($headers['Content-Type']) || $headers['Content-Type'] != 'application/json') {
+//            show_error(400, "400 Bad Request", "Invalid Content Type");
+//        }
+//        $input = file_get_contents("php://input");
+//        include('connect.php');
+//        break;
     case 'score' :
-        if ($_SERVER["REQUEST_METHOD"] == 'POST') {
-            $input = file_get_contents("php://input");
-            include('score.php');
-        } else {
+        if ($_SERVER["REQUEST_METHOD"] != 'POST') {
             show_error(405, "405 Method Not Allowed", "Invalid Method");
         }
+        if (!isset($headers['Content-Type']) || $headers['Content-Type'] != 'application/json') {
+            show_error(400, "400 Bad Request", "Invalid Content Type");
+        }
+        $input = file_get_contents("php://input");
+//        include('score.php');
+        include($service.'.php');
         break;
     case 'friend_score' :
-        if ($_SERVER["REQUEST_METHOD"] == 'GET') {
-            include('friend_score.php');
-        } else {
-            show_error(405, "405 Method Not Allowed", "Invalid Method");
-        }
-        break;
+//        if ($_SERVER["REQUEST_METHOD"] != 'GET') {
+//            show_error(405, "405 Method Not Allowed", "Invalid Method");
+//        }
+//        include('friend_score.php');
+//        break;
     case 'global_score' :
-        if ($_SERVER["REQUEST_METHOD"] == 'GET') {
-            include('global_score.php');
-        } else {
-            show_error(405, "405 Method Not Allowed", "Invalid Method");
-        }
-        break;
+//        if ($_SERVER["REQUEST_METHOD"] != 'GET') {
+//            show_error(405, "405 Method Not Allowed", "Invalid Method");
+//        }
+//        include('global_score.php');
+//        break;
     case 'country_score' :
-        if ($_SERVER["REQUEST_METHOD"] == 'GET') {
-            include('country_score.php');
-        } else {
+        if ($_SERVER["REQUEST_METHOD"] != 'GET') {
             show_error(405, "405 Method Not Allowed", "Invalid Method");
         }
+//        include('country_score.php');
+        include($service.'.php');
         break;
     default :
         show_error(503, "503 Service Unavailable", "Invalid Service");
