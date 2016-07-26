82f696c1f951133b5a4d0e1f09ff7d7e7ebe8527
chairuddinsyah <chairuddinsyah@10.100.8.149>
2016-07-26 10:50:24 +0700

    Update Commit

--- score.php
+++ score.php
@@ -13,25 +13,25 @@ if (trim($data['facebook_id']) == "") {
         "affected_row" => 0,
         "message" => "Error: facebook_id is empty"
     );
-}
-
-require 'mongodb_helper.php';
-
-$db = get_mongodb(IS_DEVELOPMENT);
-
-$document = $db->User->findOne([ 'facebook_id' => $data['facebook_id']]);
-
-$affected_row = 0;
-if (is_object($document)) {
-    if ($data['score'] > $document->score || (isset($json->overwrite) && $json->overwrite == true)) {
-        $data['updated_date'] = date('Y-m-d H:i:s');
-        $db->User->updateOne(['_id' => bson_oid((string) $document->_id)], ['$set' => $data]);
-        $affected_row = 1;
-    }
-    return array("status" => TRUE, "affected_row" => $affected_row);
 } else {
-   return array("status" => FALSE, "affected_row" => $affected_row, "message" => "User not found"); 
-}
+
+    require 'mongodb_helper.php';
+
+    $db = get_mongodb(IS_DEVELOPMENT);
+
+    $document = $db->User->findOne([ 'facebook_id' => $data['facebook_id']]);
+
+    $affected_row = 0;
+    if (is_object($document)) {
+        if ($data['score'] > $document->score || (isset($json->overwrite) && $json->overwrite == true)) {
+            $data['updated_date'] = date('Y-m-d H:i:s');
+            $db->User->updateOne(['_id' => bson_oid((string) $document->_id)], ['$set' => $data]);
+            $affected_row = 1;
+        }
+        return array("status" => TRUE, "affected_row" => $affected_row);
+    } else {
+        return array("status" => FALSE, "affected_row" => $affected_row, "message" => "User not found");
+    }
 
 //echo json_encode(array("status" => TRUE));
-
+}
